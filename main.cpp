9120585ebeba91087e97f18103a9b626474629db
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-09-21 16:49:12 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "ServerMotor0920.h"
+#include <unistd.h>//計時
+
+using namespace std;
+
+void ServerMotor0920::Reset() {//重整
+    if (error) {
+        volt = 0;
+        speed = 0;
+        current = 0;
+        error = false;
+    }
+}
+
+void ServerMotor0920::Folder(int OOP_Port) {//夾
+    while(current < 13) {
+        if (OOP_Port == 1) {
+            angle -= speed;
+        } else {
+            angle += speed;
+        }
+        speed = 7;
+        current += 1;        
+    }
+    speed = 0;
+}
+
+void ServerMotor0920::Put(int OOP_Port) {//放
+    int times = 0;//計時用
+    while(times < 3000){
+        if (OOP_Port == 1) {
+            angle += speed;
+        } else {
+            angle -= speed;
+        }
+        speed = 7;
+        sleep(1);
+        times += 1;
+    }
+    times = 0;
+    speed = 0;
+}
+
+int main() {
+    //兩邊
+    ServerMotor0920 server1;
+    ServerMotor0920 server2;
+
+    int port;//連接埠
+    cin >> port;
+    
+    server1.Reset();
+    server2.Reset();
+    server1.Folder(1);
+    server2.Folder(port);
+    server1.Put(1);
+    server2.Put(port);
+}
