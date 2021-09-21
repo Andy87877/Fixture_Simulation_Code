1f0837b9f84e967ad3c02b59537140bc7033d004
Andy8787 <86054592+Andy87877@users.noreply.github.com>
2021-09-21 16:48:52 +0800

    Create ServerMotor0920.h

---
+++ ServerMotor0920.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class ServerMotor0920 {
+    protected:
+              // 伏特      速度        角度        電流
+        double volt = 0, speed = 0, angle = 0, current = 0;
+        int OOP_Port;//連接埠
+        bool error = false;//是否錯誤
+    public:
+        void Reset();
+        void Folder(int);
+        void Put(int);
+};
+
